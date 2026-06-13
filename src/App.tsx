1880fd5416db68e67416143d2e55806c97e41a4a
Debargha Pakhira <67063441+debargha2000@users.noreply.github.com>
2026-06-13 12:29:18 +0530

    Add Vercel SpeedInsights

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppShell } from "./app/shell/AppShell";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AppShell />
+      <SpeedInsights />
     </BrowserRouter>
   );
 }
