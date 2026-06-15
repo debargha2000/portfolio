ac8921a951af8dc3b8c539b36b454e1443fa77e5
Debargha Pakhira <67063441+debargha2000@users.noreply.github.com>
2026-06-15 15:41:51 +0530

    15th

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
